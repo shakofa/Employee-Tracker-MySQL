4fe9c43cb4c8a6ace9ecdbfd4881575e1e1014ac
Anahita <shakofa.mahmody@gmail.com>
2021-03-07 17:11:01 -0800

    Inserting values in to 3 tables by hard coding in seed.sql file.

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,37 @@
-DROP DATABASE IF EXISTS employees;
-CREATE DATABASE employees;
+/*Adding the data to the tables*/
 
-USE employees;
+
+--Department table
+INSERT INTO department (name)
+Values 
+("Sales");
+("Engineering");
+("Finance");
+("Legal");
+
+
+-- Role table
+INSERT INTO role (title, salary, department_id)
+VALUES 
+("Sales Manager", 90000, 1),
+("Sales Associates", 50000, 1),
+("Software Engineers Leader", 120000, 2),
+("Mechanical Engineer", 100000, 2),
+("Finance Team Manager", 150000, 3),
+("Accountant", 95000, 3),
+("Legal department Manager", 130000, 4),
+("Lawyer", 170000, 4);
+
+
+
+--Employ table
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+("Alex", "Crouse", 1, NULL),
+("Avani", "patel", 2, 1),
+("Tom", "Sayer", 3, NULL),
+("Jasmine", "Lourd", 4, 3),
+("Abe", "Seri", 5, NULL),
+("Max", "Villiam", 6, 5),
+("Jhon", "Wek", 7, NULL),
+("Brett", "Jhonson", 8, 7);
