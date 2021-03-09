930ade6b3806343a4d8a39a6cc1e404210376952
Anahita <shakofa.mahmody@gmail.com>
2021-03-09 00:39:49 -0800

    Add two functions to add role and view role

--- index.js
+++ index.js
@@ -296,3 +296,47 @@ async function viewEmployeesByManager() {
   }
   
   
+  //Viewing roles
+  async function viewRoles() {
+    const roles = await db.findAllRoles();
+  
+    console.log("\n");
+    console.table(roles);
+  
+    loadingPrompts();
+  }
+  
+  
+  //Adding new role
+  async function addRole() {
+    const departments = await db.findAllDepartments();
+  
+    const departmentChoices = departments.map(({ id, name }) => ({
+      name: name,
+      value: id
+    }));
+  
+    const role = await prompt([
+      {
+        name: "title",
+        message: "What is the name of the role?"
+      },
+      {
+        name: "salary",
+        message: "What is the salary of the role?"
+      },
+      {
+        type: "list",
+        name: "department_id",
+        message: "Which department does the role belong to?",
+        choices: departmentChoices
+      }
+    ]);
+  
+    await db.createRole(role);
+  
+    console.log(`Added ${role.title} to the database`);
+  
+    loadingPrompts();
+  }
+  
